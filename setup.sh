41d77a55b3b06634a856835c2a4fe05cdeb18bde
Christian Mesh <christianmesh1@gmail.com>
2019-09-27 18:19:31 -0400

    Switch over to TOI github

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ -z "$BRANCH" ]; then
 		BRANCH="forge_1.12.2"
 fi
 rm -rf TrackAPI
-git clone --branch $BRANCH git@github.com:cam72cam/TrackAPI.git
+git clone --branch $BRANCH git@github.com:TeamOpenIndustry/TrackAPI.git
 ./gradlew clean
 ./gradlew cleanIdea
 ./gradlew idea
