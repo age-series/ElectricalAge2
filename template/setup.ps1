5d00c5712f907191cb0d9ec7cdbd751499590992
Christian Mesh <christianmesh1@gmail.com>
2019-09-28 10:01:59 -0400

    Update powershell script

--- template/setup.ps1
+++ template/setup.ps1
@@ -6,18 +6,28 @@ param (
 		[string]$MODCLASS
 )
 
-git clone git@github.com:TeamOpenIndustry/UniversalModCore.git --branch $BRANCH
-git clone git@github.com:TeamOpenIndustry/TrackAPI.git --branch $BRANCH
+Remove-Item -Recurse -Force src/main/java/cam72cam/$MODID/Mod.java
+Remove-Item -Recurse -Force *gradle*
 
-(Get-Content build.gradle) -replace '#MODID#', '$MODID' | Set-Content build.gradle
-(Get-Content build.gradle) -replace '#MODNAME#', '$MODNAME' | Set-Content build.gradle
-(Get-Content build.gradle) -replace '#MODVERSION#', '$MODVERSION' | Set-Content build.gradle
-New-Item -ItemType directory src/main/java/cam72cam/mod/$MODID/
-Move-Item src/main/java/cam72cam/mod/Mod.java src/main/java/cam72cam/mod/$MODID/Mod.java
+New-Item -ItemType directory src/main/java/cam72cam/$MODID/
 
-(Get-Content src/main/java/cam72cam/mod/$MODID/Mod.java) -replace '#MODID#', '$MODID' | Set-Content src/main/java/cam72cam/mod/$MODID/Mod.java
-(Get-Content src/main/java/cam72cam/mod/$MODID/Mod.java) -replace '#MODNAME#', '$MODNAME' | Set-Content src/main/java/cam72cam/mod/$MODID/Mod.java
-(Get-Content src/main/java/cam72cam/mod/$MODID/Mod.java) -replace '#MODVERSION#', '$MODVERSION' | Set-Content src/main/java/cam72cam/mod/$MODID/Mod.java
+Copy-Item -Path UniversalModCore/template/src/main/java/cam72cam/mod/Mod.java -Destination src/main/java/cam72cam/$MODID/
+Copy-Item -Path UniversalModCore/template/*gradle* -Destination $PWD -Recurse
 
-Remove-Item -Force -Recurse .git
-./gradlew.bat idea
+
+(Get-Content build.gradle) -replace "#MODID#", "$MODID" | Set-Content build.gradle
+(Get-Content build.gradle) -replace "#MODNAME#", "$MODNAME" | Set-Content build.gradle
+(Get-Content build.gradle) -replace "#MODVERSION#", "$MODVERSION" | Set-Content build.gradle
+(Get-Content src/main/java/cam72cam/mod/$MODID/Mod.java) -replace "#MODID#", "$MODID" | Set-Content src/main/java/cam72cam/mod/$MODID/Mod.java
+(Get-Content src/main/java/cam72cam/mod/$MODID/Mod.java) -replace "#MODNAME#", "$MODNAME" | Set-Content src/main/java/cam72cam/mod/$MODID/Mod.java
+(Get-Content src/main/java/cam72cam/mod/$MODID/Mod.java) -replace "#MODVERSION#", "$MODVERSION" | Set-Content src/main/java/cam72cam/mod/$MODID/Mod.java
+
+Write-Host @"
+It's recommended you now run the following for intellij setup:
+	./gradlew.bat clean
+	./gradlew.bat cleanIdea
+	./gradlew.bat classes
+	./gradlew.bat idea
+
+If you wish to use eclipse, simply replace idea with eclipse in the above commands
+"@
