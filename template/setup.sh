21f5cd662d422243c7387a86a5b53658091b5d17
Christian Mesh <christianmesh1@gmail.com>
2019-09-28 09:46:32 -0400

    Change setup messages / autosetup

--- template/setup.sh
+++ template/setup.sh
@@ -13,5 +13,12 @@ cp -r UniversalModCore/template/*gradle* .
 sed -i build.gradle -e "s/#MODID#/$MODID/" -e "s/#MODNAME#/$MODNAME/" -e "s/#MODVERSION#/$MODVERSION/"
 sed -i src/main/java/cam72cam/$MODID/Mod.java -e "s/#MODID#/$MODID/" -e "s/#MODNAME#/$MODNAME/" -e "s/#MODVERSION#/$MODVERSION/" -e "s/#MODCLASS#/$MODCLASS/"
 
-./gradlew classes
-./gradlew idea
+cat<<EOT
+It's recommended you now run the following for intellij setup:
+	./gradlew clean
+	./gradlew cleanIdea
+	./gradlew classes
+	./gradlew idea
+
+If you wish to use eclipse, simply replace idea with eclipse in the above commands
+EOT
