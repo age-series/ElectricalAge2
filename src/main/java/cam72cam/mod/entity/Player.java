680d7e9f7464a0b1866dae8fc22b881bec8edc9e
Christian Mesh <christianmesh1@gmail.com>
2019-10-13 11:18:23 -0400

    Fix #624, use saturation

--- src/main/java/cam72cam/mod/entity/Player.java
+++ src/main/java/cam72cam/mod/entity/Player.java
@@ -44,11 +44,24 @@ public class Player extends Entity {
     }
 
     public int getFoodLevel() {
-        return internal.getFoodStats().getFoodLevel();
+        return internal.getFoodStats().getFoodLevel() + (int)(internal.getFoodStats().getSaturationLevel());
     }
 
-    public void setFoodLevel(int i) {
-        internal.getFoodStats().setFoodLevel(i);
+    public void useFood(int i) {
+        int saturation = (int)(internal.getFoodStats().getSaturationLevel());
+        if (saturation > 0) {
+            int remainder = Math.max(0, i - saturation);
+            saturation = Math.max(0, saturation - i);
+            internal.getFoodStats().setFoodSaturationLevel(saturation);
+            System.out.println("Set Saturation" + saturation);
+            if (remainder == 0) {
+                return;
+            }
+
+            i = remainder;
+        }
+        System.out.println("Reduce Food");
+        internal.getFoodStats().setFoodLevel(Math.max(0, internal.getFoodStats().getFoodLevel() - i));
     }
 
     public IInventory getInventory() {
