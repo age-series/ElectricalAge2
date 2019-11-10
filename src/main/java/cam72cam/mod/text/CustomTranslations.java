182a013d84b66642986a3e3322103c10aad50f06
Christian Mesh <christianmesh1@gmail.com>
2019-11-10 14:43:26 -0500

    Whoops extra text in last commit

--- src/main/java/cam72cam/mod/text/CustomTranslations.java
+++ src/main/java/cam72cam/mod/text/CustomTranslations.java
@@ -10,7 +10,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-String lang
+
 public class CustomTranslations {
     private static Map<String, Map<String, String>> translations = new HashMap<>();
 
