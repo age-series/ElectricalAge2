f7a6d9acf905c04097e831c28567c9151d3bfe8c
Christian Mesh <christianmesh1@gmail.com>
2019-11-11 18:42:59 -0500

    Fix rider seat partial offset

--- src/main/java/cam72cam/mod/entity/ModdedEntity.java
+++ src/main/java/cam72cam/mod/entity/ModdedEntity.java
@@ -219,9 +219,9 @@ public class ModdedEntity extends Entity implements IEntityAdditionalSpawnData {
 
             Vec3d pos = calculatePassengerPosition(offset);
 
-            /* TODO 1.14.4 if (world.loadedEntityList.indexOf(seat) < world.loadedEntityList.indexOf(passenger.internal)) {
-                pos = pos.add(motionX, motionY, motionZ);
-            }*/
+            //TODO 1.14.4 if (world.loadedEntityList.indexOf(seat) < world.loadedEntityList.indexOf(passenger.internal)) {
+                pos = pos.add(new Vec3d(getMotion()));
+            //}*/
 
             passenger.setPosition(pos);
             passenger.setVelocity(new Vec3d(getMotion()));
