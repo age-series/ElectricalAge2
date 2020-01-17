027eec49fa06aa40587353e55a6ab6eaa3293adf
Jared Dunbar <jrddunbr@gmail.com>
2020-01-17 18:59:27 -0500

    moved in sim code from eln2-mc-integration

--- src/oldsim/electrical/parts/DiodeProcess.kt
+++
@@ -1,9 +0,0 @@
-package org.eln2.oldsim.electrical.parts
-
-import org.eln2.sim.IProcess
-
-open class DiodeProcess(open var resistor: ResistorSwitch) : IProcess {
-    override fun process(time: Double) {
-        resistor.state = resistor.u > 0
-    }
-}
--- src/oldsim/thermal/ThermalConnection.kt
+++
@@ -1,8 +0,0 @@
-package org.eln2.oldsim.thermal
-
-/*
-NOTE: DO NOT IMPORT MINECRAFT CODE IN THIS CLASS
-EXTEND IT INSTEAD IN THE org.eln.nbt DIRECTORY
- */
-
-class ThermalConnection(var l1: ThermalLoad, var l2: ThermalLoad)
