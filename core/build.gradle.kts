4d718d6940612d8b29df55e18e7d4a7101df0bdc
Cameron Weinfurt <weinfurt.c@hotmail.com>
2020-04-09 18:18:51 -0400

    This should fix the whole "Can't find Coroutines" problem

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     `java-library`
 }
 
+kotlin {
+    experimental.coroutines = org.jetbrains.kotlin.gradle.dsl.Coroutines.ENABLE
+}
+
 // TODO(svein): Shadow this, but do it such that we don't get one copy per subproject.
 //jar {
 //    from { configurations.compile.collect { it.isDirectory() ? it : zipTree(it) } }
