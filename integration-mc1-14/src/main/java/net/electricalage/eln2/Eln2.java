f9e4236c50c554ac474cebedf73ad2d4a04a8815
Svein Ove Aas <svein@google.com>
2020-04-13 19:13:06 +0100

    Replace various examplemod instances with eln2, and set various metadata

--- integration-mc1-14/src/main/java/net/electricalage/eln2/Eln2.java
+++ integration-mc1-14/src/main/java/net/electricalage/eln2/Eln2.java
@@ -54,7 +54,7 @@ public class Eln2
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("eln2", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
