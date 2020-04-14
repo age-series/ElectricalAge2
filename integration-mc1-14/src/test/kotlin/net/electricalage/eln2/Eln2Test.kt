4632e9c6fb0232ed7fa3d50b647198a3907e9314
Svein Ove Aas <svein@google.com>
2020-04-14 16:33:53 +0100

    Add an empty unit-test wrapper

---
+++ integration-mc1-14/src/test/kotlin/net/electricalage/eln2/Eln2Test.kt
@@ -0,0 +1,17 @@
+package net.electricalage.eln2
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+
+
+internal class Eln2Test {
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+}
