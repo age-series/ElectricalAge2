fab6a21b016a13f23fd922b64b254ca1196a83d9
Jared Dunbar <jrddunbr@gmail.com>
2020-04-17 12:54:18 -0400

    Flubber is more fun and less bugs

--- integration-vs/src/Flubber.cs
+++ integration-vs/src/Flubber.cs
@@ -4,18 +4,20 @@ using Vintagestory.API.MathTools;
 
 namespace org.Eln2 {
     public class Flubber : Block {
-        public AssetLocation tickSound = new AssetLocation("game", "tick");
+        private AssetLocation tickSound = new AssetLocation("game", "sounds/tick");
 
         public override void OnEntityCollide(IWorldAccessor world, Entity entity, BlockPos pos, BlockFacing facing, Vec3d collideSpeed, bool isImpact) {
             if (isImpact && facing.Axis == EnumAxis.Y) {
                 world.PlaySoundAt(tickSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z);
-                entity.Pos.Motion.Y *= -0.8;
-            } else if(isImpact && facing.Axis == EnumAxis.X) {
+                entity.Pos.Motion.Y *= -0.9;
+            }
+            if(isImpact && facing.Axis == EnumAxis.X) {
                 world.PlaySoundAt(tickSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z);
-                entity.Pos.Motion.X *= -0.8;
-            } else if(isImpact && facing.Axis == EnumAxis.Z) {
+                entity.Pos.Motion.X *= -5.0;
+            }
+            if(isImpact && facing.Axis == EnumAxis.Z) {
                 world.PlaySoundAt(tickSound, entity.Pos.X, entity.Pos.Y, entity.Pos.Z);
-                entity.Pos.Motion.Z *= -0.8;
+                entity.Pos.Motion.Z *= -5.0;
             }
         }
     }
