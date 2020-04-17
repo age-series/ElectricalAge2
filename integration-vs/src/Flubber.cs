240ee2b9e6475db266535de53bd2f9937ba52d3c
Jared Dunbar <jrddunbr@gmail.com>
2020-04-17 13:24:11 -0400

    Codacy wants that private readonly..

--- integration-vs/src/Flubber.cs
+++ integration-vs/src/Flubber.cs
@@ -4,7 +4,7 @@ using Vintagestory.API.MathTools;
 
 namespace org.Eln2 {
     public class Flubber : Block {
-        private AssetLocation tickSound = new AssetLocation("game", "sounds/tick");
+        private readonly AssetLocation tickSound = new AssetLocation("game", "sounds/tick");
 
         public override void OnEntityCollide(IWorldAccessor world, Entity entity, BlockPos pos, BlockFacing facing, Vec3d collideSpeed, bool isImpact) {
             if (isImpact && facing.Axis == EnumAxis.Y) {
