27a56e3b67b5b103a4ec9b0728e7b74b06e135c8
Cameron Weinfurt <weinfurt.c@hotmail.com>
2020-04-18 19:29:28 -0400

    Made proper test for ALU and fixed XNOR

--- core/src/test/kotlin/org/eln2/compute/uarchComputer/componentTests.kt
+++ core/src/test/kotlin/org/eln2/compute/uarchComputer/componentTests.kt
@@ -8,45 +8,28 @@ import org.junit.jupiter.api.Test
 class componentTests {
 	@Test
 	fun testALU() {
-		val alu = ALU(20u,40u)
-		alu.currState = ALU.ALUOps.Add
-		alu.step()
-		alu.carry = true
-		alu.currState = ALU.ALUOps.Adc
-		alu.step()
-		Assertions.assertEquals(alu.inputA, 101u)
-		alu.currState = ALU.ALUOps.Sub
-		alu.step()
-		alu.currState = ALU.ALUOps.Sbb
-		alu.step()
-		Assertions.assertEquals(alu.inputA, 21u)
-		alu.currState = ALU.ALUOps.Not
-		alu.step()
-		alu.currState = ALU.ALUOps.And
-		alu.step()
-		Assertions.assertEquals(alu.inputA, 40u)
-		alu.currState = ALU.ALUOps.Zero
-		Assertions.assertEquals(alu.inputA, 0u)
-		alu.currState = ALU.ALUOps.One
-		alu.step()
-		alu.currState = ALU.ALUOps.Shr
-		alu.step()
-		alu.currState = ALU.ALUOps.Shl
-		alu.step()
-		alu.step()
-		Assertions.assertEquals(alu.inputA, 2u)
-		alu.currState = ALU.ALUOps.Xor
-		alu.step()
-		Assertions.assertEquals(alu.inputA 42u)
-		alu.currState = ALU.ALUOps.MinusOne
-		alu.inputB = 0u
-		alu.step()
-		alu.currState = ALU.ALUOps.Nor
-		alu.step()
-		alu.currState = ALU.ALUOps.Nand
-		alu.step()
-		alu.currState = ALU.ALUOps.Xnor
-		alu.step()
-		Assertions.assertEquals(alu.inputA, 0u)
+		val alu = ALU(0u,0x42u)
+		val aluOpsToTest = arrayOf(
+			ALU.ALUOps.Add, ALU.ALUOps.Adc, ALU.ALUOps.And, ALU.ALUOps.MinusOne, ALU.ALUOps.Nand, ALU.ALUOps.Nor, ALU.ALUOps.Not,
+			ALU.ALUOps.One, ALU.ALUOps.Shl, ALU.ALUOps.Shr, ALU.ALUOps.Sub, ALU.ALUOps.Sbb, ALU.ALUOps.Xnor, ALU.ALUOps.Xor,
+			ALU.ALUOps.Zero
+		)
+		val inputAArray = arrayOf(
+			0xFFFFFFFFu, 0u, 0x41u, 0u, 0x41u, 0x41u, 0xFFFF0000u,
+			0u, 0x41u, 0x41u, 0x41u, 0x41u, 0x41u, 0x41u,
+			0xC0FFEEu
+		)
+		val expected = arrayOf(
+			0x41u, 0x43u, 0x40u, 0xFFFFFFFFu, 0xFFFFFFBFu, 0xFFFFFFBCu, 0x0000FFFFu,
+			1u, 0x82u, 0x20u, 0xFFFFFFFFu, 0xFFFFFFFFu, 0xFFFFFFFCu, 0x03u,
+			0u
+		)
+		for ( test in aluOpsToTest.zip(inputAArray.zip(expected))) {
+			alu.currState = test.first
+			alu.inputA = test.second.first
+			alu.step()
+			Assertions.assertEquals(alu.inputA, test.second.second)
+		}
+
 	}
 }
