e2a27657e67b3d0444ba6ff33df91253449d194c
Graham Northup <grissess@nexusg.org>
2020-04-23 15:40:08 -0400

    Add Documentation (first attempt) to Falstad

--- core/src/main/kotlin/org/eln2/parsers/falstad/Falstad.kt
+++ core/src/main/kotlin/org/eln2/parsers/falstad/Falstad.kt
@@ -11,18 +11,47 @@ import org.eln2.sim.electrical.mna.component.Resistor
 import org.eln2.sim.electrical.mna.component.VoltageSource
 import org.eln2.space.Set
 import org.eln2.space.Vec2i
-import java.io.FileOutputStream
-import kotlin.math.min
 
+/**
+ * A splitting pattern for runs of one or more spaces (ASCII 32).
+ *
+ * Falstad's own exporter only generates one space between parameters, which cannot contain spaces.
+ */
 val SPACES = Regex(" +")
 
+/**
+ * Refers to a pin of a [Component].
+ *
+ * In general, the pair of a Component and its index of a pin (its "own [NodeRef]") uniquely identifies the NodeRef of that component. It is not accurate to say this identifies a [Node], as the Nodes of a [Circuit] are progressively merged by connections.
+ */
 data class PinRef(val component: Component, val pinidx: Int) {
+	/**
+	 * Get the [Node] of this [PinRef].
+	 * 
+	 * This is the same Node as owned by other [NodeRef]s for [Component]s which are connected to this same "pin".
+	 */
 	val node get() = component.node(pinidx)
 }
+
+/**
+ * A simple wrapper class for a position (as a [Vec2i]) referring to a position within the Falstad canvas.
+ */
 data class PinPos(val pos: Vec2i)
 
+/**
+ * Represents a line of text from a Falstad save.
+ * 
+ * Conceptually, this is almost always a single [Component], though there are some exceptions for non-component lines (such as a "$" line, which represents global state and configuration, and some other "virtual" components like Output Probes, which we internally represent differently).
+ */
 class FalstadLine(val params: Array<String>) {
 	companion object {
+		/**
+		 * Construct a [FalstadLine] from a string representing a single line of input.
+		 * 
+		 * The string may have a terminator or not; it is trimmed in either case.
+		 * 
+		 * null is returned if the line was devoid of data (empty or consisting of only whitespace).
+		 */
 		fun fromLine(s: String): FalstadLine? {
 			val trimmed = s.trim()
 			return if(trimmed.isEmpty())
@@ -30,45 +59,116 @@ class FalstadLine(val params: Array<String>) {
 			else
 				FalstadLine(trimmed.split(SPACES).map { word -> word.trim() }.toTypedArray())
 		}
+
+		/**
+		 * Construct an iterator over [FalstadLine]s from a string representing the source of a Falstad save.
+		 */
 		fun intoLines(src: String) = src.lines().mapNotNull { fromLine(it) }
 	}
 
+	/**
+	 * Index into a Falstad save; returns the String at the given position (0 represents the start of the line).
+	 */
 	operator fun get(i: Int) = params[i]
-	
+
+	/**
+	 * Get the parameter at position [i] (0 being the first element) in the line as an Int.
+	 */
 	fun getInt(i: Int) = get(i).toInt()
+	/**
+	 * Get the parameter at position [i] (0 being the first element) in the line as a Double.
+	 */
 	fun getDouble(i: Int) = get(i).toDouble()
 }
 
+/**
+ * Aggregator class used as the parameter to [IComponentConstructor].
+ * 
+ * This class isn't intended to be used outside of the context of the construct function inside of that interface. Inside of that context, this is intended to pass any information an IComponentConstructor might need. Avoid adding arguments to the interface method itself; any additional data should be put into this class.
+ */
 data class CCData(val falstad: Falstad, val line: FalstadLine) {
+	/**
+	 * Get the underlying [Circuit] which is being constructed.
+	 */
 	val circuit get() = falstad.circuit
 
-	// Sets the conceptual number of nodes of the component--not the actual number of positions in the line!
+	/**
+	 * The number of pins on this component.
+	 * 
+	 * This is NOT the same as the positions in the data line. As far as we can tell, the positions there are the bounding box of the component on the canvas, and only map to pin positions in the orthogonal case.
+	 * 
+	 * This field can be set by an [IComponentConstructor], and specifically [PoleConstructor] will use this value to determine how many [PinRef]s should be constructed and added to the current [Falstad] deserializer. In general, this should be the same as [Component.nodeCount] for constructors which make only a single [Component].
+	 */
 	var pins: Int = 2
 
-	val pinPositions get() = (0 until min(pins, 2)).map { i -> PinPos(Vec2i(line.getInt(1 + 2 * i), line.getInt(2 + 2 * i))) }
+	/**
+	 * Get the pin positions of this component as a List<[PinPos]>.
+	 *
+	 * As far as we know, this is the bounding box of the component. This just happens to coincide with the pin positions of orthogonal bipoles.
+	 *
+	 * The exact elements consumed are indices 1 through 4 inclusive.
+	 */
+	val pinPositions get() = (0 until 2).map { i -> PinPos(Vec2i(line.getInt(1 + 2 * i), line.getInt(2 + 2 * i))) }
 
-	// Only safe to use these if pins >= 2
-	val pos: PinPos get() = pinPositions[0]
-	val neg: PinPos get() = pinPositions[1]
-	
+	/**
+	 * Get the first point of the bounding box in pinPositions.
+	 *
+	 * By convention, we consider this to be the negative terminal of a bipole, thus the name.
+	 */
+	val neg: PinPos get() = pinPositions[0]
+	/**
+	 * Get the second point of the bounding box in pinPositions.
+	 *
+	 * By convention, we consider this to be the positive terminal of a bipole, thus the name.
+	 */
+	val pos: PinPos get() = pinPositions[1]
+
+	/**
+	 * Get the integer flags argument from this constructor line.
+	 *
+	 * This is the Int value of the field at index 5.
+	 */
 	val flags: Int get() = line.getInt(5)
+
+	/**
+	 * Get the type code argument from this constructor line.
+	 *
+	 * This is the String value of the field at index 0 (the start of the line).
+	 */
 	val type: String get() = line[0]
 
+	/**
+	 * Get the data from this constructor line, as a List<String>.
+	 *
+	 * This is the sequence of Strings starting at index 6 of the line (past the flags argument).
+	 *
+	 * In Falstad's model, these are usually space-separated freeform data defined on a per-component basis.
+	 */
 	val data get() = line.params.drop(6)
 }
 
+/**
+ * A [Set] subclass which holds a [PinPos].
+ *
+ * The Set class is used to implement the Disjoint Sets algorithm; the representative of the merged sets is used to determine which PinPos' are connected by wires, and thus merged into [Node]s as present in the [Circuit].
+ */
 data class PosSet(val pos: PinPos): Set()
 
 /**
- * Component Constructor
+ * An interface for constructing [Component]s (usually)
  *
- * Interface that allows Falstad objects to be loaded from strings
+ * This is the core interface for turning a [FalstadLine] into a Component. It need not actually produce a Component; FalstadLines sometimes represent non-component data (such as global constants, or constructs with which we do not have a one-to-one mapping with our Components).
  */
 interface IComponentConstructor {
 	companion object {
-		// TODO: Custom Falstad components are a thing, so perhaps we should make this mutable?
-		//     Possibly, things could then register themselves into this? May make other things more complex
-		val TYPE_CONSTRUCTORS: Map<String, IComponentConstructor> = mapOf(
+		/**
+		 * Type constructor map, switched on the first field (the [CCData.type] code).
+		 *
+		 * This is mutable and can be registered into at runtime. There is no reentrant interface to do this at the moment.
+		 *
+		 * Generally, failing to find a typecode in here is a fatal error at construction time of a [Falstad] deserializer.
+		 */
+		val TYPE_CONSTRUCTORS: MutableMap<String, IComponentConstructor> = mutableMapOf(
 			"$" to InterpretGlobals(),
 			"o" to Ignore(),
 			"38" to Ignore(),
@@ -82,16 +182,44 @@ interface IComponentConstructor {
 			"172" to VoltageRailConstructor(),
 			"i" to CurrentSourceConstructor()
 		)
-		
+
+		/**
+		 * Returns the [IComponentConstructor] suitable for use to construct this [FalstadLine].
+		 * 
+		 * @throws IllegalStateException when the type code is not found in [TYPE_CONSTRUCTORS].
+		 */
 		fun getForLine(fl: FalstadLine) = TYPE_CONSTRUCTORS[fl[0]] ?: error("unrecognized type: ${fl[0]}")
 	}
+
+	/**
+	 * Code to generally construct a component.
+	 * 
+	 * The constructor receives a [CCData] context [ccd], which is a class specifically made to be this parameter and encapsulate all necessary context. DO NOT add parameters to this method; instead, add contextual data as fields to CCData. The context includes the line itself (as a [FalstadLine]) and the [Falstad] deserializer instance (with its [Circuit]).
+	 * 
+	 * The method is expected to mutate the context data suitable to the information found in its line; it does not return a value.
+	 */
 	fun construct(ccd: CCData)
 }
 
+/**
+ * A helper class for the common case of constructing (up to) bipoles.
+ *
+ * This abstract class implements [IComponentConstructor] and handles most of the routine parts of adding [PinRef]s to the [Falstad] context, as well as registering [Component]s with the [Circuit]. Use this class when there is a more-or-less one-to-one mapping between a [Falstad line][FalstadLine] and a [Component]; otherwise, [IComponentConstructor] is more general.
+ */
 abstract class PoleConstructor: IComponentConstructor {
+	/**
+	 * Returns the [Component] which is to be added to the [Circuit].
+	 */
 	abstract fun component(ccd: CCData): Component
+
+	/**
+	 * "Configures" the component; this usually involves initializing Component-specific data (resistance for resistors, capacitance for capactors, etc.)
+	 */
 	abstract fun configure(ccd: CCData, cmp: Component)
 
+	/**
+	 * Constructs the bipole component, using component and configure methods.
+	 */
 	override fun construct(ccd: CCData) {
 		val c = component(ccd)
 		ccd.circuit.add(c)
@@ -103,25 +231,91 @@ abstract class PoleConstructor: IComponentConstructor {
 	}
 }
 
+/**
+ * The Falstad deserializer.
+ *
+ * Given a source string as a constructor parameter, this class makes a [Circuit] corresponding to the same Falstad simulation which was exported.
+ *
+ * In general, [Component]s should have the exact same values if they can be represented at all. The state of a Component (e.g., the hidden variables of capacitors and inductors) are also restored.
+ *
+ * The Circuit should NOT be used in game logic code without some sanitization; in particular, Falstad allows for the construction of arbitrary and untrustworthy values in many locations, which can (at the very least) unbalance the game.
+ */
 class Falstad(val source: String) {
+	/**
+	 * "List of roots" (of the [Disjoint Set][Set] forest); a mapping from [PinPos] to [PosSet].
+	 *
+	 * The values of this map are members of the forest (not necessarily "roots", but the root can easily be found as the representative).
+	 */
 	val roots: MutableMap<PinPos, PosSet> = mutableMapOf()
+	/**
+	 * Get a [PosSet] from a [PinPos], registering it in [roots] if need be.
+	 *
+	 * The return value can be safely ignored if roots registration was the only goal.
+	 */
 	fun getPin(p: PinPos) = roots.getOrPut(p, { PosSet(p) })
 
+	/**
+	 * "List of refs"; a multiset from [PosSet] to [PinRef].
+	 *
+	 * As an implementation detail, a multiset is implemented as a mutable map to a mutable set.
+	 */
 	val refs: MutableMap<PosSet, MutableSet<PinRef>> = mutableMapOf()
+	/**
+	 * Get the set of [PinRef]s corresponding to this [PosSet] (creating an empty set it if it doesn't exist).
+	 */
 	fun getPinRefs(p: PosSet) = refs.getOrPut(p, { mutableSetOf() })
+	/**
+	 * Add a [PinRef] [pr] to the set of refs in [PosSet] [p]; in effect, declare that the PinRef ([Component] pin) [pr] is located at the position contained in the PosSet [p] (normally returned by [getPin]).
+	 */
 	fun addPinRef(p: PosSet, pr: PinRef) = getPinRefs(p).add(pr)
 
+	/**
+	 * "Set of grounds"; the locations of ground nodes to be unified on the canvas, encapsulated in [PosSet]s.
+	 */
 	val grounds: MutableSet<PosSet> = mutableSetOf()
+	/**
+	 * Add a ground to the ground set; in effect, declare that the position in the given [PosSet] [p] (usually via [getPin]) is grounded.
+	 */
 	fun addGround(p: PosSet) = grounds.add(p)
 
+	/**
+	 * Set of outputs; anything labelled as an "analog output" in the Falstad circuit.
+	 *
+	 * They have an arbitrary ordering, loosely based on creation order, within the save; however, their labels appear not to be saved, otherwise this would map them instead.
+	 */
 	val outputs: MutableSet<PinRef> = mutableSetOf()
+	/**
+	 * Declare [pr] as an output [PinRef].
+	 * 
+	 * Note that this does not take a [PosSet]; the PinRef is assumed to be the component against which the output is determined. If this is to be connected in the Falstad canvas, this PinRef should also be correlated with a PosSet using [addPinRef].
+	 */
 	fun addOutput(pr: PinRef) = outputs.add(pr)
+	/**
+	 * The set of output [Node]s corresponding to the output [PinRef]s.
+	 *
+	 * This can be used, e.g., to determine output potentials.
+	 */
 	val outputNodes get() = outputs.map { it.node }
 
+	/**
+	 * The globally configured Falstad timestep, which is often different from our usual 0.05s timestep (Falstad's default is 5us).
+	 */
 	var nominalTimestep = 0.05
-	// Set to false directly by various constructors that introduce explicit grounds
+	/**
+	 * Whether or not the [Circuit] is floating.
+	 *
+	 * A circuit is "floating" if it has no privileged ground reference. Circuits that do not have at least some connection to ground often underconstrain the MNA matrix and fail to iterate.
+	 *
+	 * An empty circuit is floating by definition; it stops floating when a component is constructed that provides a reference to ground. Currently, this list includes:
+	 * - Explicit grounds (element "g");
+	 * - Voltage rails (which act as a [VoltageSource] which is always grounded).
+	 * Others may arise in the future. The constructors for these components set this field to false directly.
+	 */
 	var floating = true
 
+	/**
+	 * The [Circuit] being constructed by this Falstad deserializer; this might be considered the output of this process.
+	 */
 	val circuit = Circuit()
 
 	init {
@@ -195,6 +389,17 @@ class Falstad(val source: String) {
 	}
 
 	companion object {
+		/**
+		 * Falstad's main: run a circuit description for a given number of steps.
+		 * 
+		 * This tool takes, as a command line argument, an integer number of steps, and reads a Falstad circuit from stdin. If it succeeeds, it simulates it for the given number of steps, writing a GNUplot-compatible data file to stdout which contains a timestamp (first column) followed by the node potentials (in Volts) of every output node ("analog outputs" by Falstad).
+		 * 
+		 * The output file can be redirected and plotted using a line such as `plot 'file.dat' with lines`.
+		 *
+		 * The simulation rate is taken from the Falstad description.
+		 *
+		 * Be careful when running with [DEBUG] enabled; the debugging output will intersperse into the data file.
+		 */
 		@JvmStatic
 		fun main(vararg args: String) {
 			if(args.size < 1) error("Expected number of steps")
