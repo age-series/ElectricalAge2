994fabce60777df86b0117794b8dac6f31b1f9d9
Jared Dunbar <jrddunbr@gmail.com>
2020-05-17 14:07:19 -0400

    Adds Multimeter item and client translations
    I added a new multimeter item. It doesn't really do anything, but I
    upscaled the texture and set up the language localization. According to
    the forge documentation, you can set your own translation keys, and I
    didn't see much of a convention, so I set it to
    tooltip.modid.translationtype.itemorblock.
    
    I also added the translation and tooltip to Flubber. Yay!

---
+++ integration-mc1-15/src/main/kotlin/org/eln2/items/MultimeterItem.kt
@@ -0,0 +1,21 @@
+package org.eln2.items
+
+import net.minecraft.client.util.ITooltipFlag
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.util.text.ITextComponent
+import net.minecraft.util.text.TranslationTextComponent
+import net.minecraft.world.World
+
+/**
+ * Multimeter
+ *
+ * This is used to measure the voltage of a circuit between the circuit and ground.
+ * It also displays other useful information for some components.
+ */
+class MultimeterItem: Item(Item.Properties().defaultMaxDamage(255)) {
+	override fun addInformation(stack: ItemStack, worldIn: World?, tooltip: MutableList<ITextComponent>, flagIn: ITooltipFlag) {
+		super.addInformation(stack, worldIn, tooltip, flagIn)
+		tooltip.add(TranslationTextComponent("tooltip.eln2.item.multimeter"))
+	}
+}
