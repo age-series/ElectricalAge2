eb0695f77d0b1aa14a84d87c78e3f71b489a65cb
Cameron Weinfurt <weinfurt.c@hotmail.com>
2020-05-21 16:35:32 -0400

    Added a test and some clarification for IProcess

--- core/src/main/kotlin/org/eln2/sim/IProcess.kt
+++ core/src/main/kotlin/org/eln2/sim/IProcess.kt
@@ -1,10 +1,9 @@
 package org.eln2.sim
 
-/*
-NOTE: DO NOT IMPORT MINECRAFT CODE IN THIS CLASS
-EXTEND IT INSTEAD IN THE org.eln.nbt DIRECTORY
- */
-
 interface IProcess {
-	fun process(time: Double)
+	/**
+	 * Runs the electrical simulation.
+	 * @param dt The duration of time to simulate.
+	 */
+	fun process(dt: Double)
 }
