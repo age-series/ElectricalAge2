afed4acc8411bbdd7cee841c3883c4d46a128d64
Cameron Weinfurt <weinfurt.c@hotmail.com>
2020-05-21 18:41:35 -0400

    Added requested changes

--- core/src/main/kotlin/org/eln2/sim/IProcess.kt
+++ core/src/main/kotlin/org/eln2/sim/IProcess.kt
@@ -1,8 +1,11 @@
 package org.eln2.sim
 
+/**
+ * An interface that represents a process.
+ */
 interface IProcess {
 	/**
-	 * Runs the electrical simulation.
+	 * Runs the simulation for a specified time delta.
 	 * @param dt The duration of time to simulate.
 	 */
 	fun process(dt: Double)
--- core/src/test/kotlin/org/eln2/sim/electrical/mna/IProcessTest.kt
+++ core/src/test/kotlin/org/eln2/sim/electrical/mna/IProcessTest.kt
@@ -5,15 +5,14 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 internal class IProcessTest : IProcess{
-	private var time_run : Double = 0.0;
+	private var time_run : Double = 0.0
 	@Test
 	fun iProcessTest() {
-		repeat(20) { process(0.05); } // Run the simulation 20 times with a duration of one MC tick.
-		Assertions.assertEquals(time_run, 1.0, 0.001); // Total time elapsed should be one second.
+		repeat(20) { process(0.05); } // Run the simulation 20 times for 50 ms each.
+		Assertions.assertEquals(time_run, 1.0, 0.001) // Total time elapsed should be one second.
 	}
 
 	override fun process(dt: Double) {
-		// Pretend to do the simulation
-		time_run += dt;
+		time_run += dt
 	}
 }
