75d661360c2b9c7394b1ee673bc6c24616b5b5db
Cameron Weinfurt <weinfurt.c@hotmail.com>
2020-05-22 00:32:16 -0400

    Created tests for FunctionTable and IFunction

---
+++ core/src/test/kotlin/org/eln2/math/FunctionTest.kt
@@ -0,0 +1,46 @@
+package org.eln2.math
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlin.math.*
+
+internal class Cycle(private val rate: Double) : IFunction {
+	override fun getValue(x: Double): Double {
+		return sin(this.rate * x)
+	}
+}
+
+class FunctionTest {
+	@Test
+	fun testInterface() {
+		val quarterTurn = Cycle(PI / 2.0)
+		(0 until 20).forEach { when(it % 4) {
+			0 -> Assertions.assertEquals(quarterTurn.getValue(it.toDouble()), 0.0, 0.000001)
+			1 -> Assertions.assertEquals(quarterTurn.getValue(it.toDouble()), 1.0, 0.000001)
+			2 -> Assertions.assertEquals(quarterTurn.getValue(it.toDouble()),0.0, 0.000001)
+			3 -> Assertions.assertEquals(quarterTurn.getValue(it.toDouble()),-1.0, 0.000001)
+		}}
+	}
+
+	@Test
+	fun testFunctionTable() {
+		// A sinusoid can be approximated within 200%
+		val errorTolerance = 2.0
+		// A sinusoid scaled up by two in both directions can be appoximatd within 400dd%
+		val errorToleranceAferScale = 4.0
+
+		val doublesRange = (0 until 8).map { cos(it.toDouble().times(PI).div(4.0)) }
+		val eigthTurn = FunctionTable(doublesRange.toDoubleArray(), 8.0)
+		(0 until 8).map { it.toDouble().plus(0.5) }.forEach {
+			val expected = cos(it.times(PI).div(4.0))
+			val actual = eigthTurn.getValue(it)
+			Assertions.assertTrue(actual.minus(expected).div(expected).absoluteValue < errorTolerance)
+		}
+		val eigthTurnScaled = eigthTurn.duplicate(2.0, 2.0)
+		(0 until 16).map { it.toDouble().plus(0.5) }.forEach {
+			val expected = cos(it.times(PI).div(2.0))
+			val actual = eigthTurnScaled.getValue(it)
+			Assertions.assertTrue(actual.minus(expected).div(expected).absoluteValue < errorToleranceAferScale)
+		}
+	}
+}
