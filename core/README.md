e9974b08459d2283ae643cff6ef9763e39ad5d8f
Alan Fletes <alan_09_12@outlook.com>
2021-02-15 19:00:02 -0700

    OreGen working, spawning Copper ores and flubber blocks. Item drop working, Copper ores dropping Copper Chunks and flubber blocks dropping themselves.

--- core/README.md
+++
@@ -1,4 +0,0 @@
-# eln2-core
-
-This is where all of the Eln2 core code lives. The MNA, other simulation code, etc.
-
--- core/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    `java-library`
-}
-
-// TODO(svein): Shadow this, but do it such that we don't get one copy per subproject.
-// jar {
-//    from { configurations.compile.collect { it.isDirectory() ? it : zipTree(it) } }
-// }
