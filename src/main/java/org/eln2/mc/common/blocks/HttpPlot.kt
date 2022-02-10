ed72d8a08fd903ce9326fc1b728495ff809e0fed
empireu <rodney.mckay.meredith@gmail.com>
2022-02-10 20:27:05 +0200

    remove hashset

--- src/main/java/org/eln2/mc/common/blocks/HttpPlot.kt
+++ src/main/java/org/eln2/mc/common/blocks/HttpPlot.kt
@@ -8,17 +8,22 @@ import org.apache.http.impl.client.HttpClients
 import org.eln2.mc.Eln2.LOGGER
 import org.eln2.mc.common.cell.CellGraph
 import org.eln2.mc.common.cell.CellRegistry
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
 
 object HttpPlot {
-    var attempts = 0
-    const val ATTEMPT_COUNT = 3
+    var allowed = AtomicBoolean(true)
 
     fun connectAndSend(circuit : CellGraph) {
-        if (attempts >= ATTEMPT_COUNT) {return} // Exit quickly if we've exceeded global attempt count.
+        if (!allowed.get()) {
+            return
+        }
+
         val str = Gson().toJson(JsonFrame(circuit.cells.map {
-            JsonCell(it.pos.x, it.pos.z, emptyList(),  CellRegistry.registry.getValue(it.id)!!.symbol)
+            JsonCell(it.pos.x, it.pos.z, emptyList(),  CellRegistry.registry.getValue(it.id)!!.symbol, "n/a")
         }))
+
         thread(start = true){
             val serverURL = "http://127.0.0.1:3141/"
             val post = HttpPost(serverURL)
@@ -28,20 +33,18 @@ object HttpPlot {
                 val response = client.execute(post)
                 response.close()
                 client.close()
-                attempts = 0 // reset this since there may have just been an issue with sending data the first time
             }
             catch(ex : Exception) {
                 LOGGER.warn("Was unable to send data to the test harness.")
                 // ex.printStackTrace()
-                attempts += 1 // we do 3 attempts
-                if (attempts >= ATTEMPT_COUNT) {
-                    LOGGER.warn("We are unable to send data to the test harness, and exceeded the max attempt count.")
-                }
+                allowed.set(false)
+                Thread.sleep(1000)
+                allowed.set(true)
                 return@thread
             }
         }
     }
 
-    private class JsonCell(val x : Int, val y : Int, val connections : List<Int>, val symbol : Char)
+    private class JsonCell(val x : Int, val y : Int, val connections : List<Int>, val symbol : Char, val info : String)
     private class JsonFrame(val cells : List<JsonCell>)
 }
