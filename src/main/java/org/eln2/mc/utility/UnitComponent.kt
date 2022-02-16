02a0758c0757c8b00482e405c7c70356e737f216
Jared Dunbar <jrddunbr@ja4.org>
2022-02-16 18:55:07 -0500

    hotfix

--- src/main/java/org/eln2/mc/utility/UnitComponent.kt
+++ src/main/java/org/eln2/mc/utility/UnitComponent.kt
@@ -1,9 +1,10 @@
 package org.eln2.mc.utility
 
+import net.minecraft.network.chat.BaseComponent
 import net.minecraft.network.chat.TextComponent
 
 object UnitComponent {
-    fun unitComponent(translationKey: String, value: Double, baseUnit: UnitType): TextComponent {
-
+    fun unitComponent(translationKey: String, value: Double, baseUnit: UnitType): BaseComponent {
+        return TextComponent("")
     }
 }
