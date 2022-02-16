1e8c0ca25ddbbf8c36dfca38d4237dcfd0740b39
Jared Dunbar <jrddunbr@ja4.org>
2022-02-16 18:51:34 -0500

    Feature/sim2 (#159)
     some fixes I had pending

--- src/main/java/org/eln2/mc/LibElectricTest.kt
+++ src/main/java/org/eln2/mc/LibElectricTest.kt
@@ -4,47 +4,35 @@ import org.eln2.libelectric.sim.electrical.mna.Circuit
 import org.eln2.libelectric.sim.electrical.mna.component.Resistor
 import org.eln2.libelectric.sim.electrical.mna.component.VoltageSource
 
-object LibElectricTest {
-    fun test() {
-        val thing = object: org.eln2.libelectric.sim.IProcess {
-            override fun process(dt: Double) {
-                Eln2.LOGGER.info("Got process with dt $dt")
-            }
-        }
-        thing.process(0.5)
+fun main() {
+    val circuit = Circuit()
 
-        val circuit = Circuit()
+    val vs = VoltageSource()
+    vs.potential = 100.0
+    val vsR = Resistor()
+    val resistor = Resistor()
+    val gndR = Resistor()
 
+    circuit.add(vs, vsR, resistor, gndR)
 
-        val vs = VoltageSource()
-        vs.potential = 100.0
-        val vsR = Resistor()
-        val resistor = Resistor()
-        val gndR = Resistor()
+    vs.ground(0)
+    gndR.ground(0)
 
-        circuit.add(vs, vsR, resistor, gndR)
+    // connect local VS resistor to VS
+    vsR.connect(0, vs, 1)
 
-        vs.ground(0)
-        gndR.ground(0)
+    // connect resistor to ground and voltage
+    resistor.connect(0, vsR, 1)
+    resistor.connect(1, gndR, 1)
 
-        // connect local VS resistor to VS
+    // connect ground to resistor
+    gndR.connect(1, resistor, 1)
 
-        vsR.connect(0, vs, 1)
+    // connect voltage to resistor
+    vsR.connect(1, resistor, 0)
 
-        // connect resistor to ground and voltage
-        resistor.connect(0, vsR, 1)
-        resistor.connect(1, gndR, 1)
+    // p.s pin 1 is the "public" pin
+    circuit.step(0.5)
 
-        // connect ground to resistor
-        gndR.connect(1, resistor, 1)
-
-        // connect voltage to resistor
-        vsR.connect(1, resistor, 0)
-
-        // p.s pin 1 is the "public" pin
-
-        circuit.step(0.5)
-
-        Eln2.LOGGER.info("Resistor current is ${resistor.current}")
-    }
+    Eln2.LOGGER.info("Resistor current is ${resistor.current}")
 }
--- src/main/java/org/eln2/mc/client/gui/PlotterScreen.kt
+++ src/main/java/org/eln2/mc/client/gui/PlotterScreen.kt
@@ -23,7 +23,7 @@ import kotlin.system.measureNanoTime
 class CellInfo(val type : String, val info : String, val pos : BlockPos){
     constructor(buffer : FriendlyByteBuf) : this(buffer.readString(), buffer.readString(), buffer.readBlockPos())
 
-    fun serialize(buffer : FriendlyByteBuf){
+    fun serialize(buffer : FriendlyByteBuf) {
         buffer.writeString(type)
         buffer.writeString(info)
         buffer.writeBlockPos(pos)
@@ -65,7 +65,7 @@ class PlotterScreen(private val cells : ArrayList<CellInfo>, val solveTime : Lon
     }
 
     private fun renderHeader(poseStack: PoseStack){
-        drawCenteredString(poseStack, font, "Solve time: $solveTime",width / 2 , 1, 0xFFFFFF)
+        drawCenteredString(poseStack, font, "${get(TranslatableComponent("plotter.solve_time").key)}: $solveTime",width / 2 , 1, 0xFFFFFF)
     }
 
     private val texSize = 32
@@ -195,15 +195,11 @@ class PlotterScreen(private val cells : ArrayList<CellInfo>, val solveTime : Lon
     }
 
     override fun mouseScrolled(pMouseX: Double, pMouseY: Double, pDelta: Double): Boolean {
-        val oldScale = scale
-
-        if(pDelta < 0){
+        if(pDelta < 0) {
             scale = max(0.2f, scale + pDelta.toFloat() / 20f)
-        }
-        else {
+        } else {
             scale += pDelta.toFloat() / 20f
         }
-
         return super.mouseScrolled(pMouseX, pMouseY, pDelta)
     }
 
