e5030b46f42b0569ba17048dc5ae99d7df342ed1
empireu <rodney.mckay.meredith@gmail.com>
2022-02-19 01:08:51 +0200

    rotate voxel shape mesh and new texture

--- src/main/java/org/eln2/mc/common/blocks/cell/DiodeCellBlock.kt
+++ src/main/java/org/eln2/mc/common/blocks/cell/DiodeCellBlock.kt
@@ -1,11 +1,9 @@
 package org.eln2.mc.common.blocks.cell
 
-import com.google.common.collect.ImmutableMap
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.entity.Entity
 import net.minecraft.world.level.BlockGetter
-import net.minecraft.world.level.block.RenderShape
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.shapes.BooleanOp
 import net.minecraft.world.phys.shapes.CollisionContext
@@ -13,22 +11,20 @@ import net.minecraft.world.phys.shapes.Shapes
 import net.minecraft.world.phys.shapes.VoxelShape
 import org.eln2.mc.common.blocks.CellBlockBase
 import org.eln2.mc.common.cell.CellRegistry
-import java.util.function.Function
+import org.eln2.mc.extensions.VoxelShapeExtensions.align
 
 class DiodeCellBlock: CellBlockBase() {
     override fun getCellProvider(): ResourceLocation {
         return CellRegistry.DIODE_CELL.id
     }
 
-    // TODO: This is deprecated
     override fun getCollisionShape(
         pState: BlockState,
         pLevel: BlockGetter,
         pPos: BlockPos,
         pContext: CollisionContext
     ): VoxelShape {
-        //double pX1, double pY1, double pZ1, double pX2, double pY2, double pZ2
-        return output
+        return getFor(pState)
     }
 
     // TODO: This is deprecated
@@ -38,16 +34,31 @@ class DiodeCellBlock: CellBlockBase() {
         pPos: BlockPos,
         pContext: CollisionContext
     ): VoxelShape {
-        return output
+        return getFor(pState)
     }
 
     companion object {
-        val resistorBody = box(7.0, 0.0, 0.0, 9.0, 2.0, 4.0)
-        val resistorLeadLeft = box(6.0, 0.0, 4.0, 10.0, 3.0, 12.0)
-        val resistorLeadRight = box(7.0, 0.0, 12.0, 9.0, 2.0, 16.0)
+        private val resistorBody = box(7.0, 0.0, 0.0, 9.0, 2.0, 4.0)
+        private val resistorLeadLeft = box(6.0, 0.0, 4.0, 10.0, 3.0, 12.0)
+        private val resistorLeadRight = box(7.0, 0.0, 12.0, 9.0, 2.0, 16.0)
 
-        val output = Shapes.joinUnoptimized(
+        private val shape = Shapes.joinUnoptimized(
             Shapes.joinUnoptimized(resistorLeadLeft, resistorLeadRight, BooleanOp.OR), resistorBody, BooleanOp.OR
         )
+
+        private val cache = HashMap<BlockState, VoxelShape>()
+
+        fun getFor(state : BlockState) : VoxelShape{
+            return cache.computeIfAbsent(state){
+                when(val facing = state.getValue(FACING)){
+                    Direction.NORTH -> shape
+                    Direction.SOUTH -> shape
+                    Direction.EAST -> shape.align(Direction.NORTH, Direction.EAST)
+                    Direction.WEST -> shape.align(Direction.NORTH, Direction.WEST)
+
+                    else -> throw Exception("Unhandled dir: $facing")
+                }
+            }
+        }
     }
 }
---
+++ src/main/java/org/eln2/mc/extensions/VoxelShapeExtensions.kt
@@ -0,0 +1,37 @@
+package org.eln2.mc.extensions
+
+import net.minecraft.core.Direction
+import net.minecraft.world.phys.shapes.Shapes
+import net.minecraft.world.phys.shapes.VoxelShape
+
+
+
+
+object VoxelShapeExtensions {
+    private fun indexOfDirection(dir : Direction) : Int{
+        return when(dir){
+            Direction.SOUTH -> 0
+            Direction.WEST -> 1
+            Direction.NORTH -> 2
+            Direction.EAST -> 3
+
+            else -> throw Exception("Out of bounds!")
+        }
+    }
+
+    fun VoxelShape.align(from: Direction, to: Direction): VoxelShape {
+        val buffer = arrayOf(this, Shapes.empty())
+        val times: Int = (indexOfDirection(to) - indexOfDirection(from) + 4) % 4
+
+        for (i in 0 until times) {
+            buffer[0].forAllBoxes { minX, minY, minZ, maxX, maxY, maxZ ->
+                buffer[1] = Shapes.or(buffer[1], Shapes.box(1 - maxZ, minY, minX, 1 - minZ, maxY, maxX))
+            }
+
+            buffer[0] = buffer[1]
+            buffer[1] = Shapes.empty()
+        }
+
+        return buffer[0]
+    }
+}
