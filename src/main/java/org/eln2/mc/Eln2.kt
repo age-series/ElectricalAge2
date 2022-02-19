3951e4a85cad79a13fa864112989d8c068a402aa
Jared Dunbar <jrddunbr@ja4.org>
2022-02-19 18:46:30 -0500

    Adds configuration and statistics

--- src/main/java/org/eln2/mc/Eln2.kt
+++ src/main/java/org/eln2/mc/Eln2.kt
@@ -3,18 +3,25 @@ package org.eln2.mc
 import net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.eln2.mc.common.Configuration
+import org.eln2.mc.common.ElectricalAgeConfiguration
 import org.eln2.mc.common.network.Networking
 import org.eln2.mc.common.blocks.BlockRegistry
 import org.eln2.mc.common.cell.CellRegistry
 import org.eln2.mc.common.items.ItemRegistry
+import org.eln2.mc.common.network.ModStatistics
 import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 @Mod(Eln2.MODID)
 object Eln2 {
     const val MODID = "eln2"
     val LOGGER: Logger = LogManager.getLogger()
+    val config: ElectricalAgeConfiguration
 
     init {
+        Configuration.loadConfig()
+        config = Configuration.config
+
         BlockRegistry.setup(MOD_BUS)
         ItemRegistry.setup(MOD_BUS)
 
@@ -24,5 +31,9 @@ object Eln2 {
         CellRegistry.setup(MOD_BUS)
 
         LOGGER.info("Prepared registries.")
+
+        if (config.enableAnalytics) {
+            ModStatistics.sendAnalytics()
+        }
     }
 }
