ca1db79bc11b28f66130221e3079690f272659c5
Gregory Maddra <bloxgate@gmail.com>
2022-02-21 00:29:31 -0600

    Add resistor GUI TODO: All 4 of these single value storing things could probably be reduced down to 1 class with some type parameters.

--- src/main/java/org/eln2/mc/common/containers/ContainerRegistry.kt
+++ src/main/java/org/eln2/mc/common/containers/ContainerRegistry.kt
@@ -9,6 +9,7 @@ import net.minecraftforge.registries.DeferredRegister
 import net.minecraftforge.registries.ForgeRegistries
 import net.minecraftforge.registries.RegistryObject
 import org.eln2.mc.Eln2
+import org.eln2.mc.client.screens.ResistorCellScreen
 import org.eln2.mc.client.screens.VoltageSourceCellScreen
 import org.eln2.mc.common.blocks.BlockRegistry
 
@@ -30,11 +31,20 @@ object ContainerRegistry {
             }
         }
 
+    @JvmStatic
+    val RESISTOR_CELL_CONTAINER: RegistryObject<MenuType<ResistorCellContainer>> =
+        CONTAINER_REGISTRY.register(BlockRegistry.RESISTOR_CELL.name) {
+            MenuType { id, inv ->
+                ResistorCellContainer(id, inv, inv.player)
+            }
+        }
+
 
     @SubscribeEvent
     fun clientSetup(event: FMLClientSetupEvent) {
         event.enqueueWork {
             MenuScreens.register(VOLTAGE_SOURCE_CELL_CONTAINER.get(), ::VoltageSourceCellScreen)
+            MenuScreens.register(RESISTOR_CELL_CONTAINER.get(), ::ResistorCellScreen)
         }
     }
 }
