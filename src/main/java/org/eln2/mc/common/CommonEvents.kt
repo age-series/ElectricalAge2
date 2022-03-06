737e98c550ef3c231b188459eb22b4d17e9b3ec3
Gregory Maddra <bloxgate@gmail.com>
2022-03-05 18:23:09 -0600

    Add some error handling around IO operations

--- src/main/java/org/eln2/mc/common/CommonEvents.kt
+++ src/main/java/org/eln2/mc/common/CommonEvents.kt
@@ -1,24 +1,22 @@
 package org.eln2.mc.common
 
+import com.charleskorn.kaml.Yaml
+import net.minecraft.ChatFormatting
+import net.minecraft.Util
+import net.minecraft.network.chat.TranslatableComponent
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.entity.player.Player
-import net.minecraft.network.chat.TranslatableComponent
-import net.minecraft.Util
-import net.minecraft.ChatFormatting
 import net.minecraftforge.event.ServerChatEvent
 import net.minecraftforge.event.TickEvent
 import net.minecraftforge.event.entity.EntityJoinWorldEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.loading.FMLConfig
 import net.minecraftforge.fml.loading.FMLPaths
 import net.minecraftforge.server.ServerLifecycleHooks
 import org.eln2.mc.Eln2
-import org.eln2.mc.utility.AnalyticsAcknowledgementsData
 import org.eln2.mc.common.cell.CellGraphManager
-import java.io.File
-import kotlin.io.path.Path
-import com.charleskorn.kaml.Yaml
+import org.eln2.mc.utility.AnalyticsAcknowledgementsData
+import java.io.IOException
 
 @Mod.EventBusSubscriber
 object CommonEvents {
@@ -59,20 +57,45 @@ object CommonEvents {
         if (event.world.isClientSide && event.entity is Player) {
             if (Eln2.config.enableAnalytics)  {
                 val acknowledgementFile = FMLPaths.CONFIGDIR.get().resolve("ElectricalAge/acknowledgements.yml").toFile()
-                println("Checking for file $acknowledgementFile")
                 if (!acknowledgementFile.exists()) {
                     acknowledgementFile.parentFile.mkdirs()
                     acknowledgementFile.createNewFile()
-                    acknowledgementFile.writeText(Yaml.default.encodeToString(AnalyticsAcknowledgementsData.serializer(), AnalyticsAcknowledgementsData(mutableMapOf())))
+                    try {
+                        acknowledgementFile.writeText(Yaml.default.encodeToString(AnalyticsAcknowledgementsData.serializer(), AnalyticsAcknowledgementsData(mutableMapOf())))
+                    } catch (ex: Exception) {
+                        when (ex) {
+                            is IOException, is SecurityException -> {
+                                Eln2.LOGGER.warn("Unable to write default analytics acknowledgements config")
+                                Eln2.LOGGER.warn(ex.localizedMessage)
+                            }
+                            else -> throw ex
+                        }
+                    }
                 }
                 val uuid: String = event.entity.uuid.toString()
-                val acknowledgements: AnalyticsAcknowledgementsData = Yaml.default.decodeFromStream(AnalyticsAcknowledgementsData.serializer(), acknowledgementFile.inputStream())
+                val acknowledgements: AnalyticsAcknowledgementsData = try {
+                    Yaml.default.decodeFromStream(AnalyticsAcknowledgementsData.serializer(), acknowledgementFile.inputStream())
+                } catch (ex: IOException) {
+                    AnalyticsAcknowledgementsData(mutableMapOf())
+                }
                 if (acknowledgements.entries.none { (k, _) -> k == uuid } || acknowledgements.entries[uuid]!! < (System.currentTimeMillis() / 1000L) - THIRTY_DAYS) {
                     event.entity.sendMessage(TranslatableComponent("misc.eln2.acknowledge_analytics").withStyle(ChatFormatting.RED), Util.NIL_UUID)
                     acknowledgements.entries[uuid] = System.currentTimeMillis() / 1000L
                 }
 
-                acknowledgementFile.writeText(Yaml.default.encodeToString(AnalyticsAcknowledgementsData.serializer(), acknowledgements))
+                try {
+                    acknowledgementFile.writeText(Yaml.default.encodeToString(AnalyticsAcknowledgementsData.serializer(), acknowledgements))
+                } catch (ex: Exception) {
+                    when (ex) {
+                        is IOException, is SecurityException -> {
+                            Eln2.LOGGER.warn("Unable to save analytics acknowledgements")
+                            Eln2.LOGGER.warn(ex.localizedMessage)
+
+                            event.entity.sendMessage(TranslatableComponent("misc.eln2.analytics_save_failure").withStyle(ChatFormatting.ITALIC), Util.NIL_UUID)
+                        }
+                        else -> throw ex
+                    }
+                }
             }
         }
     }
