762a2ad2a4fa96080be58a4acb13a0591d6f27c2
empireu <rodney.mckay.meredith@gmail.com>
2023-01-28 17:52:20 +0200

    Part breaking and refactored some methods in MultipartBlock

--- src/main/java/org/eln2/mc/common/blocks/MultipartBlock.kt
+++ src/main/java/org/eln2/mc/common/blocks/MultipartBlock.kt
@@ -2,20 +2,29 @@ package org.eln2.mc.common.blocks
 
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.material.FluidState
 import net.minecraft.world.level.material.Material
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.EntityCollisionContext
 import net.minecraft.world.phys.shapes.VoxelShape
 import org.eln2.mc.Eln2
+import org.eln2.mc.common.parts.Part
 
 class MultipartBlock : Block(Properties.of(Material.AIR).noOcclusion()), EntityBlock {
-    private val emptyBox = box(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    /* This is required.
+     * When minecraft tries to break the block, but no part is picked, it returns this empty box.
+     * But a completely empty box will cause some minecraft processes to fail when they try to get
+     */
+    private val epsilon = 0.00001
+    private val emptyBox = box(0.0, 0.0, 0.0, epsilon, epsilon, epsilon);
 
     override fun newBlockEntity(pPos: BlockPos, pState: BlockState): BlockEntity {
         return MultipartBlockEntity(pPos, pState)
@@ -56,32 +65,83 @@ class MultipartBlock : Block(Properties.of(Material.AIR).noOcclusion()), EntityB
         return getPartShape(pLevel, pPos, pContext)
     }
 
-    private fun getPartShape(pLevel : BlockGetter, pPos: BlockPos, pContext : CollisionContext) : VoxelShape{
-        val multipart = pLevel.getBlockEntity(pPos)
+    override fun onDestroyedByPlayer(
+        state: BlockState?,
+        level: Level?,
+        pos: BlockPos?,
+        player: Player?,
+        willHarvest: Boolean,
+        fluid: FluidState?
+    ): Boolean {
+        if(pos == null){
+            Eln2.LOGGER.error("Pos null")
+            return false
+        }
+
+        if(level == null){
+            Eln2.LOGGER.error("Destroy level null at $pos")
+            return false
+        }
+
+        if(player == null){
+            Eln2.LOGGER.error("Destroy player null at $pos")
+            return false
+        }
+
+        val multipart = level.getBlockEntity(pos) as? MultipartBlockEntity
 
         if(multipart == null){
-            Eln2.LOGGER.error("Multipart block failed to get entity at $pPos")
+            Eln2.LOGGER.error("Multipart null at $pos")
+            return false
+        }
+
+        val completelyDestroyed = multipart.remove(player, level, pos)
+
+        return if(completelyDestroyed){
+            // Only a part was removed, there are other parts in this multipart.
+
+            super.onDestroyedByPlayer(state, level, pos, player, willHarvest, fluid)
+        } else{
+            // We don't want to destroy the whole block and entity
+
+            false
+        }
+    }
+
+    private fun getPartShape(level : BlockGetter, pos: BlockPos, context: CollisionContext) : VoxelShape{
+        if(context !is EntityCollisionContext){
+            Eln2.LOGGER.error("Collision context was not an entity collision context at $pos")
             return emptyBox
         }
 
-        if(multipart !is MultipartBlockEntity){
-            Eln2.LOGGER.error("Multipart block found other entity type at $pPos")
+        if(context.entity !is LivingEntity){
             return emptyBox
         }
 
-        if(pContext !is EntityCollisionContext){
-            Eln2.LOGGER.error("Collision context was not an entity collision context at $pPos")
-            return emptyBox
-        }
+        return getPartShape(level, pos, context.entity!! as LivingEntity)
+    }
 
-        if(pContext.entity !is Player){
-            Eln2.LOGGER.error("Collision context entity was not a player at $pPos")
-            return emptyBox
-        }
-
-        val pickedPart = multipart.pickPart(pContext.entity as Player)
+    private fun getPartShape(level : BlockGetter, pos: BlockPos, entity : LivingEntity) : VoxelShape{
+        val pickedPart = pickPart(level, pos, entity)
             ?: return emptyBox
 
         return pickedPart.shape
     }
+
+    private fun pickPart(level : BlockGetter, pos : BlockPos, entity : LivingEntity) : Part?{
+        val multipart = level.getBlockEntity(pos)
+
+        if(multipart == null){
+            Eln2.LOGGER.error("Multipart block failed to get entity at $pos")
+            return null
+        }
+
+        if(multipart !is MultipartBlockEntity){
+            Eln2.LOGGER.error("Multipart block found other entity type at $pos")
+            return null
+        }
+
+        return multipart.pickPart(entity)
+    }
+
 }
