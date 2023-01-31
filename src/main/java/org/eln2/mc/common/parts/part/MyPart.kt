a40752a999e7d922aa540a0c1350daca05da4536
empireu <rodney.mckay.meredith@gmail.com>
2023-01-31 19:49:05 +0200

    CellPart server->client synchronization API

--- src/main/java/org/eln2/mc/common/parts/part/MyPart.kt
+++ src/main/java/org/eln2/mc/common/parts/part/MyPart.kt
@@ -1,15 +1,15 @@
 package org.eln2.mc.common.parts.part
 
+import net.minecraft.nbt.CompoundTag
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.phys.Vec3
 import org.eln2.mc.Eln2
 import org.eln2.mc.client.render.PartialModelPartRenderer
 import org.eln2.mc.client.render.PartialModels
-import org.eln2.mc.common.RelativeRotationDirection
-import org.eln2.mc.common.cell.CellBase
 import org.eln2.mc.common.cell.CellRegistry
 import org.eln2.mc.common.parts.*
+import java.util.concurrent.atomic.AtomicInteger
 
 class MyPart(id : ResourceLocation, context : PartPlacementContext) : CellPart(id, context, CellRegistry.WIRE_CELL.get()) {
     override val baseSize: Vec3
@@ -21,6 +21,34 @@ class MyPart(id : ResourceLocation, context : PartPlacementContext) : CellPart(i
 
     override fun createRenderer(): IPartRenderer {
         return PartialModelPartRenderer(this, PartialModels.MY_MODEL)
+    }
+
+    override fun onPlaced() {
+        super.onPlaced()
+
+        if(!placementContext.level.isClientSide){
+            syncChanges()
+        }
+    }
+
+    override fun getSyncTag(): CompoundTag? {
+        val tag = CompoundTag()
+
+        tag.putInt("Id", testId.incrementAndGet())
+
+        return tag
+    }
+
+    override fun handleSyncTag(tag: CompoundTag) {
+        assert(placementContext.level.isClientSide)
+
+        val id = tag.getInt("Id")
+
+        Eln2.LOGGER.info("Test id: $id")
+    }
+
+    companion object{
+        var testId = AtomicInteger()
     }
 
     override val allowPlanarConnections = true
