3f702774aca1c9304591fd19c2ef30fdc5449d96
empireu <rodney.mckay.meredith@gmail.com>
2023-02-17 12:15:21 +0200

    Improved WAILA handling for cells and objects, and added extensions to simplify building tooltips for LibAGE components.

--- src/main/java/org/eln2/mc/common/cells/ResistorCell.kt
+++ src/main/java/org/eln2/mc/common/cells/ResistorCell.kt
@@ -1,23 +1,13 @@
 package org.eln2.mc.common.cells
 
-import mcp.mobius.waila.api.IPluginConfig
 import net.minecraft.resources.ResourceLocation
 import org.eln2.mc.common.cells.foundation.CellBase
 import org.eln2.mc.common.cells.foundation.CellPos
 import org.eln2.mc.common.cells.foundation.objects.SimulationObjectSet
 import org.eln2.mc.common.cells.objects.ResistorObject
-import org.eln2.mc.integration.waila.IWailaProvider
-import org.eln2.mc.integration.waila.TooltipBuilder
 
-class ResistorCell(pos: CellPos, id: ResourceLocation) : CellBase(pos, id), IWailaProvider {
+class ResistorCell(pos: CellPos, id: ResourceLocation) : CellBase(pos, id) {
     override fun createObjectSet(): SimulationObjectSet {
         return SimulationObjectSet(ResistorObject())
-    }
-
-    override fun appendBody(builder: TooltipBuilder, config: IPluginConfig?) {
-        if(hasGraph){
-            builder.text("Graph", graph.id)
-        }
-        builder.text("Connections", connections.map { it.sourceDirection }.joinToString(" "))
     }
 }
