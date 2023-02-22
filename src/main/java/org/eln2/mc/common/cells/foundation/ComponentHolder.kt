6269bb6b9e34242ffd09c0592daef3b748dfd873
empireu <rodney.mckay.meredith@gmail.com>
2023-02-22 19:47:06 +0200

    Adds Polar components, fixes floating circuit issue using workaround, and adds new battery model

--- src/main/java/org/eln2/mc/common/cells/foundation/ComponentHolder.kt
+++ src/main/java/org/eln2/mc/common/cells/foundation/ComponentHolder.kt
@@ -1,7 +1,10 @@
 package org.eln2.mc.common.cells.foundation
 
 import org.ageseries.libage.sim.electrical.mna.component.Component
+import org.ageseries.libage.sim.electrical.mna.component.Port
 import org.eln2.mc.common.cells.foundation.objects.ElectricalComponentInfo
+import org.eln2.mc.common.cells.foundation.objects.ElectricalConnectionInfo
+import org.eln2.mc.common.cells.foundation.objects.ElectricalObject
 import org.eln2.mc.extensions.LibAgeExtensions.connect
 
 fun interface IComponentFactory<T> {
@@ -43,12 +46,44 @@ class ComponentHolder<T : Component>(private val factory: IComponentFactory<T>) 
         connectExternal(componentInfo.component, componentInfo.index)
     }
 
+    fun connectExternal(owner: ElectricalObject, connection: ElectricalConnectionInfo){
+        connectExternal(connection.obj.offerComponent(owner))
+    }
+
+    fun connectPositive(component: Component, remotePin: Int){
+        connect(Conventions.POSITIVE_PIN, component, remotePin)
+    }
+
+    fun connectPositive(componentInfo: ElectricalComponentInfo){
+        connectPositive(componentInfo.component, componentInfo.index)
+    }
+
+    fun connectPositive(owner: ElectricalObject, connection: ElectricalConnectionInfo){
+        connectPositive(connection.obj.offerComponent(owner))
+    }
+
+    fun connectNegative(component: Component, remotePin: Int){
+        connect(Conventions.NEGATIVE_PIN, component, remotePin)
+    }
+
+    fun connectNegative(componentInfo: ElectricalComponentInfo){
+        connectNegative(componentInfo.component, componentInfo.index)
+    }
+
+    fun connectNegative(owner: ElectricalObject, connection: ElectricalConnectionInfo){
+        connectNegative(connection.obj.offerComponent(owner))
+    }
+
     fun ground(pin: Int){
         instance.ground(pin)
     }
 
     fun groundInternal(){
         ground(Conventions.INTERNAL_PIN)
+    }
+
+    fun groundNegative(){
+        ground(Conventions.NEGATIVE_PIN)
     }
 
     fun groundExternal(){
@@ -61,6 +96,14 @@ class ComponentHolder<T : Component>(private val factory: IComponentFactory<T>) 
 
     fun offerExternal(): ElectricalComponentInfo{
         return ElectricalComponentInfo(instance, Conventions.EXTERNAL_PIN)
+    }
+
+    fun offerPositive(): ElectricalComponentInfo{
+        return ElectricalComponentInfo(instance, Conventions.POSITIVE_PIN)
+    }
+
+    fun offerNegative(): ElectricalComponentInfo{
+        return ElectricalComponentInfo(instance, Conventions.NEGATIVE_PIN)
     }
 
     fun clear() {
--- src/main/java/org/eln2/mc/extensions/LibAgeExtensions.kt
+++ src/main/java/org/eln2/mc/extensions/LibAgeExtensions.kt
@@ -1,10 +1,16 @@
 package org.eln2.mc.extensions
 
+import org.ageseries.libage.sim.electrical.mna.Circuit
 import org.ageseries.libage.sim.electrical.mna.component.Component
+import org.eln2.mc.common.cells.foundation.ComponentHolder
 import org.eln2.mc.common.cells.foundation.objects.ElectricalComponentInfo
 
 object LibAgeExtensions {
     fun Component.connect(pin: Int, info: ElectricalComponentInfo){
         this.connect(pin, info.component, info.index)
+    }
+
+    fun Circuit.add(holder: ComponentHolder<*>){
+        this.add(holder.instance)
     }
 }
