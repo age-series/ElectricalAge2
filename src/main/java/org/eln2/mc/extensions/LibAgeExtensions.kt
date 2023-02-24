9ea3437ba1d061c3d2d800daa54825842d7f6719
empireu <rodney.mckay.meredith@gmail.com>
2023-02-24 03:00:19 +0200

    Optimizes performance by not sending too many resistor updates (update using tolerance)

--- src/main/java/org/eln2/mc/extensions/LibAgeExtensions.kt
+++ src/main/java/org/eln2/mc/extensions/LibAgeExtensions.kt
@@ -2,15 +2,40 @@ package org.eln2.mc.extensions
 
 import org.ageseries.libage.sim.electrical.mna.Circuit
 import org.ageseries.libage.sim.electrical.mna.component.Component
+import org.ageseries.libage.sim.electrical.mna.component.Resistor
+import org.ageseries.libage.sim.electrical.mna.component.VoltageSource
 import org.eln2.mc.common.cells.foundation.ComponentHolder
 import org.eln2.mc.common.cells.foundation.objects.ElectricalComponentInfo
+import kotlin.math.abs
 
 object LibAgeExtensions {
+    private const val EPSILON = 0.001
+
     fun Component.connect(pin: Int, info: ElectricalComponentInfo){
         this.connect(pin, info.component, info.index)
     }
 
     fun Circuit.add(holder: ComponentHolder<*>){
         this.add(holder.instance)
+    }
+
+    fun Resistor.setResistanceEpsilon(resistance: Double, epsilon: Double = EPSILON): Boolean {
+        if(abs(this.resistance - resistance) < epsilon){
+            return false
+        }
+
+        this.resistance = resistance
+
+        return true
+    }
+
+    fun VoltageSource.setPotentialEpsilon(potential: Double, epsilon: Double = EPSILON): Boolean {
+        if(abs(this.potential - potential) < epsilon){
+            return false
+        }
+
+        this.potential = potential
+
+        return true
     }
 }
