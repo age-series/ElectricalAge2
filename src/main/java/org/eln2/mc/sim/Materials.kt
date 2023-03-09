04e354cdac4c9137125c7324e6db03d38124f2d6
empireu <rodney.mckay.meredith@gmail.com>
2023-03-09 18:12:30 +0200

    Uses BiMap instead of two maps

--- src/main/java/org/eln2/mc/sim/Materials.kt
+++ src/main/java/org/eln2/mc/sim/Materials.kt
@@ -1,20 +1,18 @@
 package org.eln2.mc.sim
 
+import org.ageseries.libage.data.biMapOf
 import org.ageseries.libage.sim.Material
 
 object MaterialMapping {
-    private val nameToMaterial = mapOf(
+    private val map = biMapOf(
         "iron" to Material.IRON
     )
 
-    private val materialToName = nameToMaterial.entries
-        .associate{ (k,v) -> v to k }
-
     fun getMaterial(name: String): Material {
-        return nameToMaterial[name] ?: error("Name $name does not correspond to any material.")
+        return map.forward[name] ?: error("Name $name does not correspond to any material.")
     }
 
     fun getName(material: Material): String {
-        return materialToName[material] ?: error("Material $material does not have a mapping!")
+        return map.backward[material] ?: error("Material $material does not have a mapping!")
     }
 }
