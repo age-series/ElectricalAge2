c4226e119c52f98c824bd8f0e85ba9a66aaa0b80
empireu <rodney.mckay.meredith@gmail.com>
2023-03-20 20:49:59 +0200

    Changes while loop to use condition instead of breaking out

--- src/main/java/org/eln2/mc/extensions/CollectionExtensions.kt
+++ src/main/java/org/eln2/mc/extensions/CollectionExtensions.kt
@@ -4,13 +4,11 @@ object CollectionExtensions {
     fun <T> ArrayDeque<T>.removeAll(action: ((T) -> Unit)): Int {
         var processed = 0
 
-        while (true){
-            if(this.isEmpty()){
-                return processed
-            }
-
+        while (!this.isEmpty()){
             action(this.removeFirst())
             processed++
         }
+
+        return processed
     }
 }
