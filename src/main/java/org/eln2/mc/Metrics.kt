7d1663d4f1559b5742e83a38d972745cf7eab7bd
empireu <rodney.mckay.meredith@gmail.com>
2023-03-22 07:54:33 +0200

    Disables prometheus

--- src/main/java/org/eln2/mc/Metrics.kt
+++ src/main/java/org/eln2/mc/Metrics.kt
@@ -1,22 +1,22 @@
 package org.eln2.mc
 
-import io.prometheus.client.exporter.HTTPServer
-import io.prometheus.client.hotspot.DefaultExports
+//import io.prometheus.client.exporter.HTTPServer
+//import io.prometheus.client.hotspot.DefaultExports
 import org.eln2.mc.Eln2.LOGGER
 
 object Metrics {
-    private val server = HTTPServer
+    /*private val server = HTTPServer
         .Builder()
         .withPort(3141)
         .build()
-
+*/
     fun initialize() {
-        DefaultExports.initialize()
+        //DefaultExports.initialize()
 
-        LOGGER.info("Initialized metrics server.")
+        //LOGGER.info("Initialized metrics server.")
     }
 
     fun destroy(){
-        server.close()
+        //server.close()
     }
 }
